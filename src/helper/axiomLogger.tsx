68c7f4736663f03ebaa636c8a25bad5343306f48
Sanskar Dahiya <sanskardahiya98@gmail.com>
2023-06-28 15:32:11 +0530

    add check for nodeEnv

--- src/helper/axiomLogger.tsx
+++ src/helper/axiomLogger.tsx
@@ -1,16 +1,17 @@
 import { log } from 'next-axiom'
-
+const NODE_ENV_NOT_PROD = process.env.NODE_ENV !== `production`
 export const logDebug = (...props: any) => {
-  console.warn(...props)
+  NODE_ENV_NOT_PROD && console.warn(...props)
+
   log.debug(props.join('-'))
 }
 
 export const logInfo = (...props: any) => {
-  console.log(...props)
+  NODE_ENV_NOT_PROD && console.log(...props)
   log.info(props.join('-'))
 }
 
 export const logError = (...props: any) => {
-  console.error(...props)
+  NODE_ENV_NOT_PROD && console.error(...props)
   log.error(props.join('-'))
 }
